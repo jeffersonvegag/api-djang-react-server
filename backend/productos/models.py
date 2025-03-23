d4dd5cced23f2972789d5175c809743086c28cc4
root <root@vmi2514655.contaboserver.net>
2025-03-23 19:45:13 +0100

    Commit inicial con todos los archivos

---
+++ backend/productos/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.nombre
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=255)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    categoria = models.ForeignKey(Categoria, related_name='productos', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre
